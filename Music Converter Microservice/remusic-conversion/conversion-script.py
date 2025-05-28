e25a3ca6a70308b11f2a69fb5e9f923c5204b370
Leonel Gonzalez <leonel.gvalencia@alumnos.udg.mx>
2025-05-27 23:28:13 -0600

    Conversion working sporadicaly - But its working

--- Music Converter Microservice/remusic-conversion/conversion-script.py
+++ Music Converter Microservice/remusic-conversion/conversion-script.py
@@ -1,7 +1,9 @@
 import json
-from kafka import KafkaConsumer
+import os
+from kafka import KafkaConsumer, KafkaProducer
 import requests
 import base64
+import subprocess
 from env_variables import (
     KAFKA_BROKER,
     INPUT_TOPIC,
@@ -11,6 +13,20 @@ from env_variables import (
     DONE_TOPIC,
     OEMER_BINARY
 )
+
+def download_image(image_url, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
+    filename = os.path.basename(image_url)
+    save_path = os.path.join(save_dir, filename)
+
+    response = requests.get(image_url, stream=True)
+    response.raise_for_status()  # Raise error if download fails
+
+    with open(save_path, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+    return save_path
 
 def get_processed_keys():
     in_progress_consumer = KafkaConsumer(INPROGRESS_TOPIC,
@@ -40,25 +56,54 @@ def encode_file_to_base64(file_path):
     with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode("utf-8")
 
+done_keys = get_processed_keys()
+print(f"Loaded processed keys: {done_keys}")
+
 def main():
     consumer = KafkaConsumer(INPUT_TOPIC,
                          group_id='my-group',
                          bootstrap_servers=[KAFKA_BROKER],
                          auto_offset_reset='earliest')
 
-    for message in consumer:
-        done_keys = get_processed_keys()
-        key_str = message.key.decode('utf-8') if message.key else ""
 
+    producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER])
+
+    for message in consumer:
+        key_str = message.key.decode('utf-8')
+
+
+        print(f"Checking key: {key_str} against done_keys: {done_keys}")
         if key_str in done_keys:
             print(f"Skipping {POST_URL + key_str}")
             continue
 
-        print(f"{message.topic} key:{key_str} value: {message.value.decode('utf-8')}")
+        producer.send(INPROGRESS_TOPIC, key=message.key, value=message.value)
+        producer.flush()
+        done_keys.add(key_str)
+        print(f"In progress STATUS of {key_str} published to {INPROGRESS_TOPIC}")
+
+        value_str = message.value.decode('utf-8')
+        data = json.loads(value_str)
+        image_url = data.get("image_path")
+
+        print(f"{message.topic} key:{key_str} value: {value_str}")
         print(f"Working on {POST_URL + key_str}")
 
-        music_xml_path = './test_files/MozaVeilSample.xml'
-        midi_path = './test_files/melody.mid'
+        image_path = download_image(image_url, "/app/temp/" + key_str)
+        print(f"Downloaded image to {image_path}")
+
+        music_xml_path = f"/app/temp/{key_str}/music_xml/{key_str}_musicxml.musicxml"
+        midi_path = f"/app/temp/{key_str}/midi/{key_str}_midi.mid"
+
+        print(f"Starting conversion process image->musicxml for {image_path}")
+        command = ["oemer", "-o", music_xml_path, image_path]
+        print(command)
+        result = subprocess.run(command, capture_output=True, check=True)
+
+        print(f"Starting conversion process musicxml->midi for {music_xml_path}")
+        command = ["python", "convert_xml_to_midi.py", f"/app/temp/{key_str}/music_xml/", f"/app/temp/{key_str}/midi/"]
+        print(command)
+        result = subprocess.run(command, capture_output=True, check=True)
 
         music_xml_base64 = encode_file_to_base64(music_xml_path)
         midi_base64 = encode_file_to_base64(midi_path)
@@ -68,15 +113,7 @@ def main():
             'music_xml_blob': music_xml_base64,
             'midi_blob': midi_base64
         }
-
-        # Publish to in-progress topic
-        # producer.produce(INPROGRESS_TOPIC, json.dumps({'key': key, 'data': payload}).encode('utf-8'))
-        # # Run oemer
-        # subprocess.run([OEMER_BINARY, "--input", data], check=True)
-        # # POST request
-        # requests.post(POST_URL, json={'key': key, 'data': data})
-        # done_keys.add(key)
-
+        print(f"Sending finished conversions to backend")
         response = requests.put(POST_URL + key_str, json=payload)
         print(f"PUT {POST_URL + key_str} status: {response.status_code}, response: {response.text}")
 
--- Music Converter Microservice/remusic-conversion/env_variables.py
+++ Music Converter Microservice/remusic-conversion/env_variables.py
@@ -5,6 +5,7 @@ POST_URL = 'http://backend:8080/sheet/'
 # Local Dev Env
 # KAFKA_BROKER = 'localhost:9092'
 # POST_URL = 'http://localhost:8080/sheet/'
+#   - ./remusic/temp:/app/temp
 
 # Kafka Info
 INPUT_TOPIC = 'newly_created'
